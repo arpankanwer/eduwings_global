e03c0a00371bdb873faa9fdfdd024081132b750b
arpankanwer <arpankanwer98@gmail.com>
2021-09-05 22:55:57 +0530

    a

--- lib/ui/resetPassword.dart
+++ lib/ui/resetPassword.dart
@@ -186,7 +186,7 @@ class _ResetPasswordState extends State<ResetPassword> {
                                               },
                                         child: isReset == false
                                             ? Text(
-                                                'Reset Passsword',
+                                                'Reset',
                                                 style: Theme.of(context)
                                                     .textTheme
                                                     .headline1,
